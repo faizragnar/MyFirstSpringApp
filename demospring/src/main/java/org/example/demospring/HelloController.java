5efeb4c638b74bf1d0b21c042b2bd5bb88cea43c
unknown <faiyazamail@gmail.com>
2025-03-01 21:15:26 +0530

    [Faiyaj ahmed] Add . Print hello with rest controller

---
+++ demospring/src/main/java/org/example/demospring/HelloController.java
@@ -0,0 +1,20 @@
+package org.example.demospring;
+
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping(value = {"", "/", "/home"})
+    public String sayHello() {
+        return "Hello From BridgeLabz!!!";
+    }
+
+  
+    }
+}
---
+++ demospring/src/test/java/org/example/demospring/DemospringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
