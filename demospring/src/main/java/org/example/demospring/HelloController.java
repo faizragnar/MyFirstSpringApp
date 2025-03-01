cf09d37153ab43a2c8e111383d6c504eb32fa47b
unknown <faiyazamail@gmail.com>
2025-03-01 21:26:32 +0530

    [Faiyaj ahmed] Add . param

--- demospring/src/main/java/org/example/demospring/HelloController.java
+++ demospring/src/main/java/org/example/demospring/HelloController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @RequestMapping("/hello")
@@ -17,6 +19,10 @@ public class HelloController {
 
     @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
     public String sayHello( @RequestParam (value = "name") String name) {
+        return "Hello " + name + "!";
+    }
+    @GetMapping("/param/{name}")
+    public String sayHelloParam(@PathVariable String name) {
         return "Hello " + name + "!";
     }
 }
